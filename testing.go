60ab7b338cce00647a0900cfe48cfc00093bb071
Soe Tint Aung <soetintaunghz10@gmail.com>
2024-08-14 12:04:38 +0630

    truffle

---
+++ testing.go
@@ -0,0 +1,12 @@
+package abc
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Deliberately include a fake API key
+	apiKey := "sk_test_4eC39HqLyjWDarjtT1zdp7dc"
+	fmt.Println("Hello, World!")
+	fmt.Println("API Key:", apiKey)
+}
